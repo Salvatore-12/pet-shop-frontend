3192ce173a04fc9c32fa3c084b66339f0c0ed5d1
Salvatore Assennato <137400679+Salvatore-12@users.noreply.github.com>
2024-02-12 18:16:58 +0100

    aggiornamenti nella stuttura  della pagina Login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,29 +1,58 @@
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import { Col, Container, Row } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
-const Login=()=> {
+const Login = () => {
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
+    <Container
+      className="immagine fw-bold text-success w-100 fw-bolder "
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Row className="justify-content-evenly align-align-items-center w-50 h-50 mb-1 mt-5">
+        <Col>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" placeholder="inserisci l'Email" />
+              <Form.Text className="text-muted"></Form.Text>
+            </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="inserisci la Password"
+              />
+            </Form.Group>
+            <Row>
+              <Col md={2} className="justify-content-center">
+                <Button
+                  className="text-bg-success text-black border-0 fw-medium"
+                  type="submit"
+                >
+                  Accedi
+                </Button>
+              </Col>
+
+              <Col md={2}>
+                <Button
+                  className="text-bg-success text-black border-0 fw-medium"
+                  type="submit"
+                >
+                  Registrati
+                </Button>
+              </Col>
+            </Row>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
   );
-}
+};
 
 export default Login;
